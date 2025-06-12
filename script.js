acea6ef5a03dc7326af57440b0b8b869db11709d
josephhongg <joseph.hong99@gmail.com>
2025-06-12 16:48:42 +1200

    html, css & js changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const apiUrl = "https://restcountries.com/v3.1/name/";
-const searchBox = document.querySelector(".entry-area input");
+const searchBox = document.querySelector("input");
 const searchBtn = document.querySelector("button");
 const information = document.querySelector(".information");
 
@@ -12,12 +12,26 @@ const getCountry = async (country) => {
 	// if data returns 404 or "Not Found" show alert and reset searchBox. else, populate with country's data
 	if (data.status == "404" || data.message == "Not Found") {
 		alert("Please enter valid country");
-		searchBox.value =  "";
+		searchBox.value = "";
 	} else {
-		document.querySelector(".information img").src = data[0].flags.png;
-		document.querySelector(".information .name").innerHTML = data[0].name.common;
-		document.querySelector(".information .capital").innerHTML = data[0].capital;
-		document.querySelector(".information .population").innerHTML = "Population: " + data[0].population;
+		const flag = document.createElement("img");
+		flag.src = data[0].flags.png;
+		information.appendChild(flag);
+
+		const name = document.createElement("h2");
+		name.classList.add("name")
+		name.innerHTML = data[0].name.common;
+		information.appendChild(name)
+
+		const capital = document.createElement("div");
+		capital.classList.add("capital");
+		capital.innerHTML = data[0].capital;
+		information.appendChild(capital)
+
+		const population = document.createElement("div");
+		population.classList.add("population");
+		population.innerHTML = "Population: " + data[0].population;
+		information.appendChild(population)
 
 		// reset the search box
 		searchBox.value = "";
